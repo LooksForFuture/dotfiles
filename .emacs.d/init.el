d06acc3567da582149d1e276d154667e0ae594d5
Armin <56917818+LooksForFuture@users.noreply.github.com>
2026-09-11 13:08:26 +0330

    Update init.el

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -1,8 +1,10 @@
+;; -*- lexical-binding: nil; -*- 
+
 ;; Pakcage management
 (require 'package)
 
 (add-to-list 'package-archives
-	     '("melpa" . "http://melpa.org/packages/") t)
+	     '("melpa" . "https://melpa.org/packages/") t)
 
 (package-initialize)
 (when (not package-archive-contents)
@@ -11,21 +13,22 @@
 ;; Installs packages
 (defvar myPackages
   '(
-    zenburn-theme
+    use-package
+    try
     spacemacs-theme
     treesit-auto
-    treemacs
     vterm
     magit
     slime
     paredit
     rainbow-delimiters
-   )
-)
-(mapc #'(lambda (package)
-          (unless (package-installed-p package)
-            (package-install package)))
-      myPackages)
+    anzu
+    org-tree-slide
+    )
+  )
+(dolist (package myPackages)
+  (unless (package-installed-p package)
+    (package-install package)))
 
 ;; Customization
 (setq make-backup-files nil)
@@ -36,153 +39,121 @@
 (setq display-line-numbers-type 'relative) 
 (global-display-line-numbers-mode)
 
+(setq tab-bar-show 1)
+
 (setq-default indent-tabs-mode t)
 (setq c-ts-mode-indent-offset 8)
+(setq c-ts-mode-indent-style 'linux)
+(setq c-default-style "linux")
 (setq c++-ts-mode-indent-offset 8)
 (setq cmake-ts-mode-indent-offset 8)
+(setq imenu-auto-rescan t)
 
-(global-set-key (kbd "C-<left>")  'windmove-left)
-(global-set-key (kbd "C-<right>") 'windmove-right)
-(global-set-key (kbd "C-<up>")    'windmove-up)
-(global-set-key (kbd "C-<down>")  'windmove-down)
-(global-set-key [f4]  'compile)
+(global-set-key (kbd "C-<return>")  'vterm)
+(global-set-key (kbd "M-<left>")  'windmove-left)
+(global-set-key (kbd "M-<right>") 'windmove-right)
+(global-set-key (kbd "M-<up>")    'windmove-up)
+(global-set-key (kbd "M-<down>")  'windmove-down)
+(global-set-key [f2]  'compile)
 (global-set-key [f1]  'project-compile)
 
 (setq show-paren-delay 0)
 (show-paren-mode)
 
 ;; Disabling things
-;;-----------------------------------------------------------------------
-(menu-bar-mode -1) 
-(toggle-scroll-bar -1) 
-(tool-bar-mode -1) 
-(setq default-frame-alist '((undecorated . t)))
+;;-----------------------------------------------------
+(menu-bar-mode -1)
+(toggle-scroll-bar -1)
+(tool-bar-mode -1)
+(add-to-list 'default-frame-alist '(undecorated . t))
+(delete-selection-mode 1)
 
 ;; Configure SBCL as the Lisp program for SLIME.
 (add-to-list 'exec-path "/usr/local/bin")
 (setq inferior-lisp-program "sbcl")
 
-;;Treemacs config
-(use-package treemacs
-  :ensure t
-  :defer t
-  :init
-  (with-eval-after-load 'winum
-    (define-key winum-keymap (kbd "M-0") #'treemacs-select-window))
-  :config
-  (progn
-    (setq treemacs-buffer-name-function            #'treemacs-default-buffer-name
-          treemacs-buffer-name-prefix              " *Treemacs-Buffer-"
-          treemacs-deferred-git-apply-delay        0.5
-          treemacs-directory-name-transformer      #'identity
-          treemacs-display-in-side-window          t
-          treemacs-eldoc-display                   'simple
-          treemacs-file-extension-regex            treemacs-last-period-regex-value
-          treemacs-file-name-transformer           #'identity
-          treemacs-follow-after-init               t
-          treemacs-expand-after-init               t
-          treemacs-find-workspace-method           'find-for-file-or-pick-first
-          treemacs-git-command-pipe                ""
-          treemacs-goto-tag-strategy               'refetch-index
-          treemacs-header-scroll-indicators        '(nil . "^^^^^^")
-          treemacs-hide-dot-git-directory          t
-          treemacs-indentation                     2
-          treemacs-indentation-string              " "
-          treemacs-is-never-other-window           nil
-          treemacs-max-git-entries                 5000
-          treemacs-missing-project-action          'ask
-          treemacs-move-files-by-mouse-dragging    t
-          treemacs-move-forward-on-expand          nil
-          treemacs-no-png-images                   nil
-          treemacs-no-delete-other-windows         t
-          treemacs-project-follow-cleanup          nil
-          treemacs-persist-file                    (expand-file-name ".cache/treemacs-persist" user-emacs-directory)
-          treemacs-position                        'left
-          treemacs-read-string-input               'from-child-frame
-          treemacs-recenter-distance               0.1
-          treemacs-recenter-after-file-follow      nil
-          treemacs-recenter-after-tag-follow       nil
-          treemacs-recenter-after-project-jump     'always
-          treemacs-recenter-after-project-expand   'on-distance
-          treemacs-litter-directories              '("/node_modules" "/.venv" "/.cask")
-          treemacs-project-follow-into-home        nil
-          treemacs-show-cursor                     nil
-          treemacs-show-hidden-files               t
-          treemacs-silent-filewatch                nil
-          treemacs-silent-refresh                  nil
-          treemacs-sorting                         'alphabetic-asc
-          treemacs-select-when-already-in-treemacs 'move-back
-          treemacs-space-between-root-nodes        t
-          treemacs-tag-follow-cleanup              t
-          treemacs-tag-follow-delay                1.5
-          treemacs-text-scale                      nil
-          treemacs-user-mode-line-format           nil
-          treemacs-user-header-line-format         nil
-          treemacs-wide-toggle-width               70
-          treemacs-width                           30
-          treemacs-width-increment                 1
-          treemacs-width-is-initially-locked       t
-          treemacs-workspace-switch-cleanup        nil)
-
-    ;; The default width and height of the icons is 22 pixels. If you are
-    ;; using a Hi-DPI display, uncomment this to double the icon size.
-    (treemacs-resize-icons 15)
-
-    (treemacs-follow-mode t)
-    (treemacs-filewatch-mode t)
-    (treemacs-fringe-indicator-mode 'always)
-    (when treemacs-python-executable
-      (treemacs-git-commit-diff-mode t))
-
-    (pcase (cons (not (null (executable-find "git")))
-                 (not (null treemacs-python-executable)))
-      (`(t . t)
-       (treemacs-git-mode 'deferred))
-      (`(t . _)
-       (treemacs-git-mode 'simple)))
-
-    (treemacs-hide-gitignored-files-mode nil))
-  :bind
-  (:map global-map
-        ("M-0"       . treemacs-select-window)
-        ("C-x t 1"   . treemacs-delete-other-windows)
-        ("C-x t t"   . treemacs)
-        ("C-x t d"   . treemacs-select-directory)
-        ("C-x t B"   . treemacs-bookmark)
-        ("C-x t C-t" . treemacs-find-file)
-        ("C-x t M-t" . treemacs-find-tag)))
-
-(treemacs-start-on-boot)
-
-; Treesitter
+;; Treesitter
 (use-package treesit-auto
   :config
   (setq treesit-auto-install 'prompt) ;; auto-install grammars
-  (global-treesit-auto-mode))
+)
 
 (add-to-list 'auto-mode-alist '("CMakeLists\\.txt\\'" . cmake-ts-mode))
 
+;; Anzu
+(global-anzu-mode +1)
+
 ;; Enable Paredit.
-(add-hook 'emacs-lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'eval-expression-minibuffer-setup-hook 'enable-paredit-mode)
-(add-hook 'ielm-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-interaction-mode-hook 'enable-paredit-mode)
-(add-hook 'lisp-mode-hook 'enable-paredit-mode)
-(add-hook 'slime-repl-mode-hook 'enable-paredit-mode)
-(require 'paredit)
+(use-package paredit
+  :hook ((emacs-lisp-mode
+          lisp-mode
+          slime-repl-mode
+          ielm-mode) . enable-paredit-mode)
+  :defer t)
 (defun override-slime-del-key ()
   (define-key slime-repl-mode-map
     (read-kbd-macro paredit-backward-delete-key) nil))
 (add-hook 'slime-repl-mode-hook 'override-slime-del-key)
 
 ;; Enable Rainbow Delimiters.
-(add-hook 'emacs-lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'ielm-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-interaction-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'slime-repl-mode-hook 'rainbow-delimiters-mode)
+(use-package rainbow-delimiters
+  :hook ((prog-mode . rainbow-delimiters-mode)))
 
-(add-hook 'c-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'c++-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'c-ts-mode-hook 'rainbow-delimiters-mode)
-(add-hook 'c++-ts-mode-hook 'rainbow-delimiters-mode)
+;; Dired
+(setq dired-kill-when-opening-new-dired-buffer t)
+(put 'dired-find-alternate-file 'disabled nil)
+(add-hook 'dired-mode-hook #'auto-revert-mode)
+(setq dired-listing-switches "-alh --group-directories-first")
+
+(defun my/switch-to-dired ()
+  "Switch to an existing dired buffer, or open a new one."
+  (interactive)
+  (let ((buf (seq-find
+              (lambda (b)
+                (with-current-buffer b
+                  (derived-mode-p 'dired-mode)))
+              (buffer-list))))
+    (if buf
+        (switch-to-buffer buf)
+      (call-interactively #'dired))))
+
+(defun my/dired-preview-file ()
+  "Preview file in other window without leaving dired."
+  (interactive)
+  (let ((file (dired-get-file-for-visit)))
+    (when (file-regular-p file)
+      (view-file-other-window file))))
+
+(global-set-key (kbd "C-x d") #'my/switch-to-dired)
+(with-eval-after-load 'dired
+  (define-key dired-mode-map (kbd "RET") #'dired-find-file)
+  (define-key dired-mode-map (kbd "^")
+	      (lambda () (interactive) (find-alternate-file "..")))
+  (define-key dired-mode-map (kbd "TAB") #'my/dired-preview-file))
+
+;; Magit
+(use-package magit
+  :bind ("C-x g" . magit-status)
+  :defer t)
+
+;; org-mode
+(with-eval-after-load "org-tree-slide"
+  (define-key org-tree-slide-mode-map (kbd "C-,") 'org-tree-slide-move-previous-tree)
+  (define-key org-tree-slide-mode-map (kbd "C-.") 'org-tree-slide-move-next-tree)
+  )
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(acme-theme anzu magit mathjax monokai-theme org-tree-slide paredit
+		rainbow-delimiters slime spacemacs-theme treesit-auto
+		try vterm)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
